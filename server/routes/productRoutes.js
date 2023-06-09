ee2ada1c62a93b802300108023f3b8e6f4ce2647
Azzam Adnan <azzamuadonang@gmail.com>
2023-06-09 08:49:51 +0700

    serverless non upload img

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -5,8 +5,10 @@ const {upload} = require('../middleware/configUpload');
 productRoute.get('/', ProductController.getProduct);
 productRoute.get('/detail/:id',ProductController.detail)
 productRoute.get('/search',ProductController.search);
-productRoute.post('/create',upload, ProductController.createProduct);
-productRoute.put('/update/:id',upload, ProductController.updateProduct);
+// productRoute.post('/create',upload, ProductController.createProduct);
+productRoute.post('/create', ProductController.createProduct); // non uploading serverless
+// productRoute.put('/update/:id',upload, ProductController.updateProduct);
+productRoute.put('/update/:id', ProductController.updateProduct); // non uploading serverless
 productRoute.delete('/delete/:id', ProductController.deleteProduct);
 productRoute.get('/pagination', ProductController.getProductsPagination);
 
